7671f32e7075f192c055a58174e2274fc5d0bded
Hasibullah Yosufi <95396667+Khesorw@users.noreply.github.com>
2022-01-15 21:22:43 -0500

    First commit initialized workspace into the repository

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+
+
+
+    char str[] = "Hello";
+
+    if(strcmp(str,"Hello\n") == 0){
+
+
+        printf("String are the same \":)\"");
+    }else {printf("String are not the same\n");
+
+    }
+
+    if(strstr(str,"Hell")){
+        printf("containg string boggs");
+    }
+
+
+    /*
+    char str1[100] = "Hello I am Hasib";
+    char str[] = " What is your name?";
+    char copyStr[63];
+   // strcpy(copyStr, str1);
+   // strcat(str1, str);
+   // puts(str1);
+    strcat(str1," another concatination for the string Woop woop");
+     puts(str1);
+      strcpy(str1,"String are copied and taht is enough");
+      puts(str1);
+
+      */
+
+
+
+
+    /*
+
+    char str[] = "Hellow world";
+    char str2[35];
+    strcat(str2,str);
+    puts(str2);
+    */
+
+    /*
+    int k;
+    k=23;
+    printf("enter value");
+    scanf("%d",&k);
+    printf("%d",k);
+    char sdf[] = "Hellowe asdf";
+    printf("Hello world!\n");
+    printf("Hello From cdsadfasdfads");*/
+    return 0;
+}
